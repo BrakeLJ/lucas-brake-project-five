d4ff1ddfcbce23a642f3c3333bb278a9b49a27ec
BrakeLJ <lukejbrake@gmail.com>
2020-09-30 14:51:47 -0400

    Updates

---
+++ precache-manifest.9c96da80fcadacf96a55074bf7b8d176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67b8a4e41927c7db3944ef1c73cddb2",
+    "url": "/lucas-brake-project-five/index.html"
+  },
+  {
+    "revision": "3bd57edd13c19dfe0c6a",
+    "url": "/lucas-brake-project-five/static/css/main.67d499a0.chunk.css"
+  },
+  {
+    "revision": "38a08828b647db29f65b",
+    "url": "/lucas-brake-project-five/static/js/2.611774b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lucas-brake-project-five/static/js/2.611774b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd57edd13c19dfe0c6a",
+    "url": "/lucas-brake-project-five/static/js/main.57df0ed1.chunk.js"
+  },
+  {
+    "revision": "6fbf74c1df369e9ad750",
+    "url": "/lucas-brake-project-five/static/js/runtime-main.b19370d3.js"
+  },
+  {
+    "revision": "7e490caaea8c3743b039329afc8b0fab",
+    "url": "/lucas-brake-project-five/static/media/marvel-banner.7e490caa.jpg"
+  }
+]);
